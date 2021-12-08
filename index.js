721eb42ffb42cb114b27d872166d9224f6072cb8
Fred <higgins.fred.s@gmail.com>
2021-12-07 20:29:17 -0800

    commit tree 1c4d92502488cb7bb89cde67da47c49ed828772c

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution here
+//Variable for testing purposes
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+// Finds matching drivers
+function findMatching(array, name) {
+    const matchingName = array.filter(arr => {
+        return arr.toLowerCase() === name.toLowerCase();
+    })
+    return matchingName;
+}
+
+//Searches by first letters
+function fuzzyMatch(array, name) {
+    const matchingName = array.filter(arr => {
+        return arr.startsWith(name);
+    });
+    return matchingName;
+}
+
+//
+function matchName(object, name) {
+    const matchingName = object.filter(obj => {
+        return obj.name === name;
+    });
+    return matchingName;
+}
+
+//Test output
+console.log(matchName(drivers, "Sammy"))
